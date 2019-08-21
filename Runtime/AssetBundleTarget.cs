437595f27f9f6d9eb3671e50a92826ed7b2ed43c
David LeGare <dlegare.1001@gmail.com>
2019-08-21 16:31:09 -0500

    Cleanup AssetBundleTarget enum
    * Remove incorrect WSAPlayer variant.
    * Remove explicit values from variants.
    * Remove inaccurate paragraph in documentation.

--- Runtime/AssetBundleTarget.cs
+++ Runtime/AssetBundleTarget.cs
@@ -6,29 +6,18 @@ namespace SynapseGames.AssetBundle
     /// </summary>
     ///
     /// <remarks>
-    /// <para>
     /// The purpose of this target is to identify which target platform an asset bundle
     /// can be used on. When building bundles, the <see cref="UnityEditor.BuildTarget"/>
     /// enum is used, however that enum is not available at runtime. This enum allows us
     /// to use the same platform identifiers at built time and runtime.
-    /// </para>
-    ///
-    /// <para>
-    /// The value of each variant matches the corresponding variant in
-    /// <see cref="UnityEditor.BuildTarget"/>, so it is safe to cast directly between
-    /// <see cref="AssetBundleTarget"/> and <see cref="UnityEditor.BuildTarget"/>. Note,
-    /// though, that this enum does not include all of the older, deprecated variants
-    /// in <see cref="UnityEditor.BuildTarget"/>.
-    /// </para>
     /// </remarks>
     public enum AssetBundleTarget
     {
-        StandaloneOSX = 2,
-        StandaloneWindows = 5,
-        StandaloneLinux = 17,
-        iOS = 9,
-        Android = 13,
-        WebGL = 20,
-        WSAPlayer = 21,
+        StandaloneOSX,
+        StandaloneWindows,
+        StandaloneLinux,
+        iOS,
+        Android,
+        WebGL,
     }
 }
